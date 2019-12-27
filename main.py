d783e68ce19a6df9d9e2bbd6a559c7a6f5d3ce6f
yarastqt <yarastqt@gmail.com>
2019-12-28 01:17:03 +0300

    chore: impl simple read json

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+import json
+
+try:
+  with open("./fixtures/package.json") as file:
+    data = json.load(file)
+  print(data["name"])
+except IOError:
+    print("At this folder package.json not found, please call from root.")
